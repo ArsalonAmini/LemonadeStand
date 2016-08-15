e2ee9cd33705d8768aae4b0511b42c624de90395
ArsalonAmini <aminihajibasa@gmail.com>
2016-08-15 18:12:23 -0500

    End of 8/15/2016 work customer array needs to be understood, recipe class needs logic, player needs a scoring function for profit/budget , Inventory getLemons need methods that link to player budget, lemonade stand should prompt user on purchasing inventory, day class needs logic and weather should be linked to days, game needs a 7 day instantiation

--- LemonadeStand/LemonadeStand/GamePlay/Customer.cs
+++ LemonadeStand/LemonadeStand/GamePlay/Customer.cs
@@ -7,33 +7,64 @@ using System.Threading.Tasks;
 namespace LemonadeStand
 {
 
-//    public class Customer : PriceChanceOfBuying : WeatherChanceOfBuying 
-//    {
-//        int weatherChance;
-//        int priceDemand;
- 
-//        public Customer(int weatherChance, int priceDemand) //constructor
-//        {
-//            this.weatherChance = weatherChance;
-//            this.priceDemand = priceDemand;
-//        }
+    public class Customer
+    {
+        int weatherChance;
+        int priceDemand;
+        int price;
 
-//    public class PriceChanceOfBuying(int 
-//    public int BuyerChance()
-//    {
-//        buyerChance = weatherChance + priceDemand;
-//    }
-
-//    public int BuyLemonade()
-//    {
-//        if (buyerChance > 20)
-//        {
-//            Player.profit = +1 //add lemonade sale profit to variable profit (player class)
-//            inventory.Lemonade - 1 //reduce lemonade inventory by 1 (3 lemons, 2 sugars and 4 ices) in (inventory class)
-//        }
-//    }
-//}
+        public Customer() //constructor 
+        {
+            this.weatherActual = Weather weather;
+            this.price = LemonadeStand Setprice;
+        }
 
 
-//member variables - numberofCups, ChangeOfBuying (PricePoint), 
-//member methods - .BuyLemonade (int cups,) , ChanceOfBuyingPrice(), ChanceOfBuyingWeather()
+        public int CalculatePriceChanceOfBuying() //Method
+        {
+            if (setPrice > 5) //price score from lemonadeStand class player input
+            {
+                priceDemand = Random.Next(1, 10);
+            }
+            else if (setPrice < 3)
+            {
+                priceDemand = Random.Next(50, 100);
+            }
+        }
+
+        public int CalculateWeatherChanceOfBuying(Weather weather)
+        {
+            if (weather = 1) //weather score from weather class associated with Rain
+            {
+                chanceWeather = Random.Next(1, 10);
+            }
+            else if (weather < 1) //weather score from weather class associated with Sunshine
+            {
+                chanceWeather = Random.Next(20, 100);
+            }
+            else
+            {
+                chanceWeather = Random.Next(1, 10);
+            }
+        }
+
+
+        public int CalculateTotalCustomerChanceOfBuying(int chanceWeather, int priceDemand)
+        {
+            totalCustomerChance = chanceWeather + priceDemand;
+        }
+
+        //public int BuyLemonade() //Pass in customerFleet member variable customer and
+        //If criteria is met, make array of customers purchase or not purchase or whatever
+        //{
+        //    if (totalCustomerChance > 20)
+        //    {
+        //        Player.profit = +1 //add lemonade sale profit to variable profit (player class)
+        //        inventory.Lemonade - 1 //reduce lemonade inventory by 1 (3 lemons, 2 sugars and 4 ices) in (inventory class)
+        //}
+        //}
+    }
+}
+
+//member variables - numberofCups, ChangeOfBuying(PricePoint), 
+//member methods - .BuyLemonade(int cups,) , ChanceOfBuyingPrice(), ChanceOfBuyingWeather()
--- LemonadeStand/LemonadeStand/GamePlay/LemonadeStand.cs
+++ LemonadeStand/LemonadeStand/GamePlay/LemonadeStand.cs
@@ -8,15 +8,49 @@ namespace LemonadeStand.GamePlay
 {
     class LemonadeStand
     {
-        //member variables - customer (object), Inventory (object)
-        //member methods - SetPrice
+        int predictedWeather; //Member Variables
+
+        public LemonadeStand() //constructor
+        {
+            Player player = new Player();
+            Customer customer = new Customer();
+            Inventory inventory = new Inventory();
+            Store store = new Store();
+            Weather weather = new Weather();
+            Day day = new Day();
+        }
+
+
+        public int SetPrice()
+        {
+            Console.WriteLine("Enter an integer value cooresponding to today's selling price");
+            setPrice = parse.Int32(Console.ReadLine()); //convert userInput string to integer
+        }
+
+        public void PredictWeather()
+        {
+            predictedWeather = Random.Next(1, 10)
+
+            if (predictedWeather > 7)
+            {
+                Console.WriteLine("The predicted forcast for the next 7 days is sunny");
+            }
+            else
+            {
+                Console.WriteLine("The predicted forcast for the next 7 days is raining!");
+            }
+        }
+
+        public void CalculateCustomer()
+        {
+            customer.CalculateWeatherChanceOfBuying(weather);
+            customer.CalculatePriceChanceOfBuying(SetPrice);
+        }
+
+        //public void RunStand() //Method for running the stand
+        //{
+        //    Player. something 
+
+        //}
     }
-
-
-    public int SetPrice() 
-    {
-        Console.WriteLine("Enter an integer value cooresponding to today's selling price");
-            parse.Int32 = Console.ReadLine(); //convert userInput string to integer
-    }
-
 }
