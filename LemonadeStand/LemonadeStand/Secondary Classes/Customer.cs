955169987f6cd16d16664151b58c072efd5b385d
ArsalonAmini <aminihajibasa@gmail.com>
2016-08-22 08:29:32 -0500

    working MakeCustomerBuyLemonade member method

--- LemonadeStand/LemonadeStand/Secondary Classes/Customer.cs
+++ LemonadeStand/LemonadeStand/Secondary Classes/Customer.cs
@@ -10,22 +10,11 @@ namespace LemonadeStand
     {
         Random random;
         public int customerPreference;
+        public List<Customer> potentialCustomers;
 
         public Customer() //constructor
         {
             this.random = new Random();
-        }
-
-        public List<Customer> GeneratePotentialCustomers(int numberOfPotentialCustomers) //Verified and Tested by A Amini-Hajibashi 8/20/2016
-
-        {
-            List<Customer> potentialCustomers = new List<Customer>();
-            for (int i = 0; i < numberOfPotentialCustomers; i++)
-            {
-                Customer customer = new Customer();
-                potentialCustomers.Add(new Customer());
-            }
-            return potentialCustomers;
         }
 
         public List<Customer> CalculateCustomerPreference(List<Customer> potentialCustomers) //verified by A.Amini-Hajibashi, built on 8/20/2016
@@ -37,40 +26,12 @@ namespace LemonadeStand
             return potentialCustomers;
         }
 
-        public void MakeCustomerBuyLemonade(List<Customer> potentialCustomers) //tested and verifed by A.Amini-Hajibashi  8/22/2016
-        {
-            foreach (Customer customer in potentialCustomers)
-            {
-                if (customer.customerPreference == 1)
-                {
-                    //recipe for sweet
-                    Console.WriteLine("Customer bought sweet lemonade!");
-                    //return potentialCustomers;
-                }
-                else if (customer.customerPreference == 2)
-                {
-                    //recipe tart
-                    //tartRecipe.MakeLemonade();
-                    Console.WriteLine("Customer bought a tart lemonade!");
-                    //return potentialCustomers;
-                }
-                else if (customer.customerPreference == 3)
-                {
-                    Console.WriteLine("Customer did not buy lemonade!");
-                }
-                else
-                {
-                    //recipe for normal
-                    //recipe.MakeLemonade();
-                    Console.WriteLine("Customer didn't buy lemonade!");
-                    //return potentialCustomers;
-                }
-            }
+        
 
         }
     }
 
-}
+
 
 
 
--- LemonadeStand/LemonadeStand/Secondary Classes/Day.cs
+++ LemonadeStand/LemonadeStand/Secondary Classes/Day.cs
@@ -20,8 +20,8 @@ namespace LemonadeStand
         Tertiary_Classes.Recipe recipe;
         Tertiary_Classes.TartRecipe tartRecipe;
         Tertiary_Classes.SweetRecipe sweetRecipe;
-        private int customerPreference;
-       
+        
+
         int numberOfPotentialCustomers = 100;
 
         public Day()
@@ -41,19 +41,19 @@ namespace LemonadeStand
         public void RunDay()
         {
 
-            customer.GeneratePotentialCustomers(numberOfPotentialCustomers);
+            GeneratePotentialCustomers(numberOfPotentialCustomers);
             cashBox.PrintBalance();
             //this.PredictWeather();
             //inventory.GetInventory();
             player.BuyLemons();
-            player.BuySugar();                           
+            player.BuySugar();
             player.BuyIce();
             weather.ActualWeather();
             recipe.MakeLemonade();
             tartRecipe.MakeLemonade();
             sweetRecipe.MakeLemonade();
-            customer.CalculateCustomerPreference(potentialCustomers);
-            customer.MakeCustomerBuyLemonade(potentialCustomers);
+            CalculateCustomerPreference(potentialCustomers);
+            MakeCustomerBuyLemonade(potentialCustomers);
 
             //salesLoop //recipe.AddPlayerMoney(cashBox); //RecipeActsOnCashBox
             //Foreach potential customer do they buy or not buy
@@ -64,50 +64,104 @@ namespace LemonadeStand
 
         }
 
-       
+
+        public List<Customer> GeneratePotentialCustomers(int numberOfPotentialCustomers) //Verified and Tested by A Amini-Hajibashi
+
+        {
+      
+            for (int i = 0; i < numberOfPotentialCustomers; i++)
+            {
+                Customer customer = new Customer();
+                potentialCustomers.Add(new Customer());
+            }
+            return potentialCustomers;
+
         }
-        //public void PredictWeather() //tested and verified by A Amini-Hajibashi on 8/17/2016
-        //{
-        //    Random random = new Random();
-        //    int predictedWeather = random.Next(1, 100);
 
-        //    if (predictedWeather > 50)
-        //    {
-        //        Console.WriteLine("The predicted forcast for the next 7 days is sunny");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("The predicted forcast for the next 7 days is raining!");
-        //    }
-        //}
+        public List<Customer> CalculateCustomerPreference(List<Customer> potentialCustomers) //verified by A.Amini-Hajibashi, built on 8/20/2016
+        {
+            foreach (Customer customer in potentialCustomers) //does this function gen a list of objects with customer preference attribute??
+            {
+                customer.customerPreference = random.Next(1, 4);
+            }
+            return potentialCustomers;
+        }
 
-        //public int ActualWeather() //Tested and verified by A Amini-Hajibashi on 8/19/2016
-        //{
-        //    int actualWeather = random.Next(1, 100);
+        public void MakeCustomerBuyLemonade(List<Customer> potentialCustomers) //tested and verifed by A.Amini-Hajibashi  8/22/2016
+        {
+            foreach (Customer customer in potentialCustomers)
+            {
+                if (customer.customerPreference == 1)
+                {
+                    //recipe for sweet
+                    Console.WriteLine("Customer bought sweet lemonade!");
+                    //return potentialCustomers;
+                }
+                else if (customer.customerPreference == 2)
+                {
+                    //recipe tart
+                    //tartRecipe.MakeLemonade();
+                    Console.WriteLine("Customer bought a tart lemonade!");
+                    //return potentialCustomers;
+                }
+                else if (customer.customerPreference == 3)
+                {
+                    Console.WriteLine("Customer did not buy lemonade!");
+                }
+                else
+                {
+                    //recipe for normal
+                    //recipe.MakeLemonade();
+                    Console.WriteLine("Customer didn't buy lemonade!");
+                    //return potentialCustomers;
+                }
+            }
+        }
+    }
+    //public void PredictWeather() //tested and verified by A Amini-Hajibashi on 8/17/2016
+    //{
+    //    Random random = new Random();
+    //    int predictedWeather = random.Next(1, 100);
 
-        //    if (actualWeather > 50)
-        //    {
-        //        weather = 1;
-        //        Console.WriteLine("It's sunny");
-        //        return weather;
-        //    }
-        //    else if (actualWeather < 50)
-        //    {
-        //        weather = 0;
-        //        Console.WriteLine("It's raining");
-        //        return weather;
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("The weather is partly sunny today");
-        //        weather = 2;
-        //        return weather;
-        //    }
-        //}
+    //    if (predictedWeather > 50)
+    //    {
+    //        Console.WriteLine("The predicted forcast for the next 7 days is sunny");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("The predicted forcast for the next 7 days is raining!");
+    //    }
+    //}
 
-    
+    //public int ActualWeather() //Tested and verified by A Amini-Hajibashi on 8/19/2016
+    //{
+    //    int actualWeather = random.Next(1, 100);
+
+    //    if (actualWeather > 50)
+    //    {
+    //        weather = 1;
+    //        Console.WriteLine("It's sunny");
+    //        return weather;
+    //    }
+    //    else if (actualWeather < 50)
+    //    {
+    //        weather = 0;
+    //        Console.WriteLine("It's raining");
+    //        return weather;
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("The weather is partly sunny today");
+    //        weather = 2;
+    //        return weather;
+    //    }
+    //}
+
+
 
 }
+
+
 
 
 
